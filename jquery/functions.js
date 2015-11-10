7452de00c5840f8b78a93f76747fe0f0edfd9fff
Rustykiler <max.beekmans@hotmail.com>
2015-11-10 16:47:29 +0100

    website is almost done it's time for content

---
+++ jquery/functions.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+
+	var $root = $('html, body');
+	$('a').click(function() {
+	    $root.animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+	
+});
